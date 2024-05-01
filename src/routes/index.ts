d26831243adae84abc2c4bb1c1307664e4293b22
gufranlazuardi <gufranlazuardi@gmail.com>
2024-05-02 01:25:07 +0700

    feat: routes

---
+++ src/routes/index.ts
@@ -0,0 +1,24 @@
+import { createRouter, createWebHistory } from "vue-router";
+import Home from "../pages/Home.vue";
+import Sample from "@/pages/Sample.vue";
+import SampleDua from "@/pages/SampleDua.vue";
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    {
+      path: "/",
+      component: Home,
+    },
+    {
+      path: "/sample",
+      component: Sample,
+    },
+    {
+      path: "/sampledua",
+      component: SampleDua,
+    },
+  ],
+});
+
+export default router;
